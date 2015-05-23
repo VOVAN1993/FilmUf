a318cdcc581011248ddfdb0fc0329cb0111a8473
PazRosemarie <ufimtseva336@mail.ru>
2015-05-23 05:55:32 +0300

    continue

---
+++ app/src/main/java/com/example/Start/activity/RegistrationActivity.java
@@ -0,0 +1,14 @@
+package com.example.Start.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.Start.R;
+
+public class RegistrationActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+    }
+}
